948bd326cdd54595272cb6e9c6391aa2b8c2180d
AlexMaxBrown <vitalik_gavrilov_94@mail.ru>
2019-06-12 17:39:30 +0400

    Version v2.1

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,82 @@
-$(function(){new Swiper(".swiper-container",{loop:!0,spaceBetween:30,centeredSlides:!0,autoplay:{delay:5600,disableOnInteraction:!1},pagination:{el:".swiper-pagination",clickable:!0}}),$('a[href="#search"]').on("click",function(e){e.preventDefault(),$("#search").addClass("open"),$('#search > form > input[type="search"]').focus()}),$("#search, #search button.close").on("click keyup",function(e){e.target!=this&&"close"!=e.target.className&&27!=e.keyCode||$(this).removeClass("open")}),$("form").submit(function(e){return e.preventDefault(),!1})});
+$(function() {
+    new Swiper(".swiper-container", {
+        loop: !0,
+        spaceBetween: 30,
+        centeredSlides: !0,
+        autoplay: {
+            delay: 5600,
+            disableOnInteraction: !1
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: !0
+        }
+    }), $('a[href="#search"]').on("click", function(e) {
+        e.preventDefault(), $("#search").addClass("open"), $('#search > form > input[type="search"]').focus()
+    }), $("#search, #search button.close").on("click keyup", function(e) {
+        e.target != this && "close" != e.target.className && 27 != e.keyCode || $(this).removeClass("open")
+    }), $("form").submit(function(e) {
+        return e.preventDefault(), !1
+    })
+	jQuery('.prev').on('click', function (e) {
+		e.stopImmediatePropagation();
+		var btn_group_parent = $(this).closest('.btn-group');
+		var number = 0;
+		var show_number = btn_group_parent.find('.show-number');
+		var a = show_number.text();
+		a = parseInt(a);
+		if (a > 1) {
+			number = a - 1;
+		} else {
+			number = 1;
+		}
+		show_number.text(number);
+
+	});
+
+	jQuery('.next').on('click', function (e) {
+		e.stopImmediatePropagation();
+		var btn_group_parent = $(this).closest('.btn-group');
+		var number = 0;
+		var show_number = btn_group_parent.find('.show-number');
+		var a = show_number.text();
+		a = parseInt(a);
+		if (a > 0) {
+			number = a + 1;
+		}
+		show_number.text(number);
+	});
+
+	// trim polyfill : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/Trim
+	if (!String.prototype.trim) {
+		(function() {
+			// Make sure we trim BOM and NBSP
+			var rtrim = /^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g;
+			String.prototype.trim = function() {
+				return this.replace(rtrim, '');
+			};
+		})();
+	}
+
+	[].slice.call( document.querySelectorAll( 'input.input__field' ) ).forEach( function( inputEl ) {
+		// in case the input is already filled..
+		if( inputEl.value.trim() !== '' ) {
+			classie.add( inputEl.parentNode, 'input--filled' );
+		}
+
+		// events:
+		inputEl.addEventListener( 'focus', onInputFocus );
+		inputEl.addEventListener( 'blur', onInputBlur );
+	} );
+
+	function onInputFocus( ev ) {
+		classie.add( ev.target.parentNode, 'input--filled' );
+	}
+
+	function onInputBlur( ev ) {
+		if( ev.target.value.trim() === '' ) {
+			classie.remove( ev.target.parentNode, 'input--filled' );
+		}
+	}
+
+});
